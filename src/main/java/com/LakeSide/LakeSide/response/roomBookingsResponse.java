944c35ce4a988ea4861a69aed52dc47ab2064134
Al3k5a24 <leki.djekic@gmail.com>
2025-12-01 23:41:18 +0100

    Added response for room booking history

---
+++ src/main/java/com/LakeSide/LakeSide/response/roomBookingsResponse.java
@@ -0,0 +1,130 @@
+package com.LakeSide.LakeSide.response;
+
+import com.LakeSide.LakeSide.model.Room;
+import com.LakeSide.LakeSide.model.UserAccount;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class roomBookingsResponse {
+    private Long id;
+
+    private Room room;
+
+    private UserAccount user;
+
+    private String guestFullName;
+
+    private String guestEmail;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private int numOfAdults;
+
+    private int numOfChildren;
+
+    private int totalNumOfGuests;
+
+    private String bookingConfirmationCode;
+
+    private BigDecimal totalPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public UserAccount getUser() {
+        return user;
+    }
+
+    public void setUser(UserAccount user) {
+        this.user = user;
+    }
+
+    public String getGuestFullName() {
+        return guestFullName;
+    }
+
+    public void setGuestFullName(String guestFullName) {
+        this.guestFullName = guestFullName;
+    }
+
+    public String getGuestEmail() {
+        return guestEmail;
+    }
+
+    public void setGuestEmail(String guestEmail) {
+        this.guestEmail = guestEmail;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public int getNumOfAdults() {
+        return numOfAdults;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+    }
+
+    public int getNumOfChildren() {
+        return numOfChildren;
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+    }
+
+    public int getTotalNumOfGuests() {
+        return totalNumOfGuests;
+    }
+
+    public void setTotalNumOfGuests(int totalNumOfGuests) {
+        this.totalNumOfGuests = totalNumOfGuests;
+    }
+
+    public String getBookingConfirmationCode() {
+        return bookingConfirmationCode;
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
